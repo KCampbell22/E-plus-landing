af1dfa316237d7eabc8085d8626bc3197abc5b79
KCampbell22 <ksoup12@gmail.com>
2022-06-20 09:10:33 -0500

    need to start making branches

---
+++ nav.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelectorAll('.nav-links');
+const navLinks = document.querySelector('.nav-items');
+const mobileNav = document.querySelector('.mobile-nav');
+const mobileItems = document.querySelector('.mobile-items');
+
+function showNav() {
+  
+   
+        mobileNav.classList.toggle('show');
+    
+}
+
